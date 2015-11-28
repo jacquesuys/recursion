286f094e4997513bd7a0ee8d00b11bb5851b6739
Jacques Uys <jaxuys@gmail.com>
2015-11-28 15:04:57 +0200

    A recursive DOM looper ala Douglas Crockford, that evaluates to true. Still think there is potential for cleanup over here.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+	// your code here
+	var classes = [];
+	var regex = new RegExp(' ' + className + ' ');
+
+	var walkDOM = function (node,func) {
+        func(node);
+
+        node = node.firstChild;
+        while(node) {
+            walkDOM(node,func);
+            node = node.nextSibling;
+        }
+    };
+
+    walkDOM(document.body, function(node) {
+    	if (regex.test(' ' + node.className + ' ')) {
+    		classes.push(node);
+    	}
+    });
+
+    return classes;
 };
