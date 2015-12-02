930f5764ce31aa1af35b8870301364afb5f0fbc6
Jacques Uys <jaxuys@gmail.com>
2015-12-02 22:24:57 +0200

    My first stab at the this thing.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,52 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	'use strict';
+
+	// Reliable type checking: http://bit.ly/1l8NvS4
+	var getType = function(obj) {
+		return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+	};
+
+	var isType = getType(obj);
+
+	var isString = function (string) {
+		return string.replace(/['"]+/g, "");
+    };
+
+    var each = function(collection, iterator) {
+        if (getType(collection) === "array")
+            for (var index = 0; index < collection.length; index++)
+                iterator(collection[index], index, collection);
+        else if (getType(collection) === "object" && getType(collection) !== "null")
+            for (var key in collection)
+                iterator(collection[key], key, collection);
+    };
+
+    var oldArr = [];
+	if (isType === "array") {
+		if(obj.length === 0) {
+			return '[]';
+		} else {
+			each(obj, function(value) {
+				oldArr.push(value);
+			});
+
+			return '[' + oldArr.join(',') + ']';
+		}
+	}
+
+	if (isType === "function") {
+
+	}
+
+	if (isType === "string") {
+
+		return isString(obj);
+	}
+
+	if (isType === "number" || "boolean" || "null") {
+		// console.log(getType);
+		return String(obj);
+	}
 };
