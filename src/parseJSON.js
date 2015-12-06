03c405b833f4b977fe7413342a7700536b679a92
Jacques Uys <jaxuys@gmail.com>
2015-12-06 07:55:54 +0200

    This alone makes all the test cases pass?

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,12 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+	'use strict';
+
+	// Resources consulted: http://bit.ly/1jH6EJ9
+	// In Doug's own words: 
+	// JavaScript structure. The '{' operator is subject to a syntactic ambiguity
+	// in JavaScript: it can begin a block or an object literal. We wrap the text
+	// in parens to eliminate the ambiguit
+	return eval('(' + json + ')');
 };
